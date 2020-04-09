470d8d3d16481f179edc315003fec22750ea520f
Nicholas Roy <nicholas.roy12@hotmail.co.uk>
2020-04-09 14:07:21 +0100

    Adding notes

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ const app = require('./server')
 const dotenv = require('dotenv')
 dotenv.config()
 
+// .env was not included in git clone, will need to re-establish connection string before working
+
 mongodb.connect(process.env.DB_CONNECTION_STRING, { useUnifiedTopology: true }, function (err, client) {
   if (err) throw new Error(err)
   app.set('db', client.db())
